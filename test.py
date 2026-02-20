abe049d46dac98402b837c57f68466de5005c7d4
Frederic Perreau <Frederic.Perreau@kyndryl.com>
2026-02-20 22:24:42 +0100

    test.py initial

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    parser = argparse.ArgumentParser(description='Process some strings.')
+    parser.add_argument('strings', metavar='mot', type=str, nargs='+',help='a string to print')
+    args = parser.parse_args()
+
+    # Process strings if provided
+    if args.strings:
+        for string in args.strings:
+            print(f'"{string}"', end=' ')
+        print()
+
