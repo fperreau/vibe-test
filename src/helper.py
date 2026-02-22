894836ad951c4588623e4a49fd4502c62c5dffd1
Frederic Perreau <Frederic.Perreau@kyndryl.com>
2026-02-22 19:00:32 +0100

    refactoring generate python scripts

--- src/helper.py
+++ src/helper.py
@@ -1,47 +1,18 @@
-#! /usr/bin/env python.exe
-##
-#   NAS Helper Script
-#
-#   New-Alias incus .\helper.py
-#   Remove-Alias incus
-#
-##
-
 import typer
-from nas import NAS
-from share import Share
-from user import User
 
 app = typer.Typer()
 nas_app = typer.Typer()
 share_app = typer.Typer()
 user_app = typer.Typer()
 
-app.add_typer(nas_app, name="nas")
-nas_app.add_typer(share_app, name="share")
-nas_app.add_typer(user_app, name="user")
 
-@nas_app.command()
-def list():
-    NAS.list()
-@nas_app.command()
-def create(name: str):
-    NAS.create(name)
-@nas_app.command()
-def delete(name: str):
-    NAS.delete(name)
-@nas_app.command()
-def use(name: str):
-    NAS.use(name)
-@share_app.command()
-def list():
-    Share.list()
-@share_app.command()
-def create(share_name: str, source: str, share_type: str):
-    Share.create(share_name, source, share_type)
-@share_app.command()
-def delete(name: str):
-    Share.delete(name)
+
+#
+# CLI - user
+#
+
+user_app = typer.Typer()
+
 @user_app.command()
 def list():
     User.list()
@@ -54,5 +25,40 @@ def delete(user: str):
 @user_app.command()
 def owner(user: str, share: str):
     User.owner(user, share)
-if __name__ == "__main__":
-    app()
+
+#
+# Helper Class
+#
+class Helper:
+    name = None
+
+    def __init__(self, name):
+        self.name = name
+        
+    @staticmethod 
+    def create(name: str):
+        Helper.name = name 
+
+#
+# User Class
+#
+class User:
+    dUsers = {}
+
+    def __init__(self, name):
+        User.dUsers[name] = self
+
+    @staticmethod
+    def list():
+        typer.echo("Listing users")
+        for user in User.dUsers:
+            typer.echo(user)
+
+    @staticmethod
+    def create(user: str, password: str):
+        typer.echo(f"Creating user: {user} with password {password}")
+
+    @staticmethod
+    def delete(user: str):
+        typer.echo(f"Deleting user: {user}")
+        User.dUsers.pop(user)
--- src/nas.py
+++ src/nas.py
@@ -1,12 +1,59 @@
+#! /usr/bin/env python.exe
+##
+#   NAS Helper Script
+#
+#   New-Alias incus .\helper.py
+#   Remove-Alias incus
+##
+
 import typer
+import helper as HR
 
-class Helper:
-    name = None
+#
+# CLI - nas [user, share]
+#
+app_nas = typer.Typer()
+app_nas.add_typer(HR.user_app, name="user")
 
-    def __init__(self, name):
-        self.name = name
+share_app = typer.Typer()
+app_nas.add_typer(share_app, name="share")
 
-class NAS(Helper):
+@app_nas.command()
+def list():
+    NAS.list()
+
+@app_nas.command()
+def create(name: str):
+    NAS.create(name)
+
+@app_nas.command()
+def delete(name: str):
+    NAS.delete(name)
+
+@app_nas.command()
+def use(name: str):
+    NAS.use(name)
+
+#
+# Share Class
+#
+@share_app.command()
+def list():
+    Share.list()
+
+@share_app.command()
+def create(share: str, source: str, type: str):
+    Share.create(share, source, type)
+
+@share_app.command()
+def delete(name: str):
+    Share.delete(name)
+
+#
+# NAS Class
+#
+class NAS(HR.Helper):
+    
     def __init__(self, name):
         super().__init__(name)
 
@@ -16,13 +63,56 @@ class NAS(Helper):
 
     @staticmethod
     def create(name: str):
-        NAS.name = name
+        HR.Helper.create(name)
         typer.echo(f"Creating NAS device: {name}")
 
     @staticmethod
     def delete(name: str):
         typer.echo(f"Deleting NAS device: {name}")
 
+#
+#  Share Class
+#
+class Share(NAS):
+    dShare = {} # Use a dictionary to store shares by name
+
+    def __init__(self, share, source, mount_point, user, type):
+        super().__init__(NAS.name)
+        Share.dShare[share] = self
+        self.source = source
+        self.mount_point = mount_point
+        self.user = user
+        self.type = type
+    
+    def __str__(self):
+        return f"share: {self.name}, source: {self.source}, mount_point: {self.mount_point}, owner: {self.user}, type: {self.type}"
+
     @staticmethod
-    def use(name: str):
-        typer.echo(f"Using NAS device: {name}")
+    def list():
+        typer.echo("Listing shares")
+        i = 0
+        for obj in Share.dShare.values():
+            i += 1
+            typer.echo(f"id: {i}, {obj.__str__()}")
+   
+
+    @staticmethod
+    def create(name: str, source: str, mount_point: str, user=None, type="cifs"):
+        obj = Share(name, source, mount_point, user, type)
+        Share.dShare[name] = obj
+        typer.echo(f"Created share: {obj.__str__()}")
+
+    @staticmethod
+    def delete(name: str):
+
+        typer.echo(f"Deleting share: {name}")
+
+    @staticmethod
+    def owner(user: str):
+        typer.echo(f"Setting user {user} as owner of share {share}")
+
+#
+#   Main
+#
+if __name__ == "__main__":
+    app_nas()
